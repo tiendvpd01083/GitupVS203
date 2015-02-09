0e0204ab8e713d154c5b6bac3cac3611b8817a62
tiendvpd01083 <tiendvpd01083@fpt.edu.vn>
2015-02-09 11:33:00 +0700

    1

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
 
     End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
